87f12748fa7ff423986e2127e41a7b0319d00369
Yannick <yannickzahinda442@gmail.com>
2024-10-07 01:31:43 +0200

    add style to dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,10 @@ class HomeController < ApplicationController
         @holy_days = HolyDay.all 
         @reports = Report.all
         @users = User.all
+        @total_reports = Report.count
+        @total_meetings = Meeting.count
+        @total_sick_leaves = SickLeave.count 
+        @total_presents_employees = Attendance.where(status: 'present').count
+        @total_absents_employees = Attendance.where(status: 'absent').count
     end
 end
