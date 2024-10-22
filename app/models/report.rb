4378468ecca5ed70b4d9a5cd89f7550e8e1041d3
Yannick <yannickzahinda442@gmail.com>
2024-10-22 08:59:33 +0200

    add rapport type to reports

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,7 @@
 class Report < ApplicationRecord
     belongs_to :user
+    has_many :attachments
+    validate :title, presence: true
+    validates :content, presence: true
+    validates :report_type, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_21_114046) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_22_065655) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_21_114046) do
     t.string "title"
     t.text "content"
     t.integer "user_id"
+    t.string "report_type"
   end
 
   create_table "sick_leaves", force: :cascade do |t|
